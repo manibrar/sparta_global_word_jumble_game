3ff57529a1df4e9caafc715cb0919fd37105dd5e
manibrar <manvirbrar1988@gmail.com>
2018-06-19 02:29:17 +0100

    working panels start and undo buttons

--- js/index2.js
+++ js/index2.js
@@ -3,15 +3,15 @@ console.log("Hello Word Jumble");
 var myMainHeader = document.getElementById('mainHeader');
 var myStartButton = document.getElementById('startButton')
 var myJumbledPanel = document.querySelectorAll('.panel1');
-var myGuessPreview = document.querySelectorAll('#guessPreview');
+var myGuessPreview = document.getElementById('guessPreview');
 var mySubmitButton = document.getElementById('submitButton')
+var myUndoButton = document.getElementById('undoButton')
 var wordLibrary = ["WordJumble"];
-
-var wordInLetters = ["W","O","R","D","J","U","M","B","L","E"];
+var wordInLetters = ["W", "O", "R", "D", "J", "U", "M", "B", "L", "E"];
 var guesses = [];
 
-
-myStartButton.addEventListener('click',function(e){
+//Start button and transfer letters into panels
+myStartButton.addEventListener('click', function(e) {
   myStartButton.innerHTML = "Game Started!";
   myStartButton.style.backgroundColor = "orange";
   for (var i = 0; i < wordInLetters.length; i++) {
@@ -19,21 +19,38 @@ myStartButton.addEventListener('click',function(e){
   }
 });
 
+
+//Guess preview with maximum length condition
 for (var i = 0; i < myJumbledPanel.length; i++) {
-myJumbledPanel[i].addEventListener('click',function(e){
-  if (guesses.length >= wordInLetters.length) {
-    alert("Maximum Letters")
-  }else{
-  var para = document.createElement("row");
+  myJumbledPanel[i].addEventListener('click', function(e) {
+    if (guesses.length >= wordInLetters.length && e.target.innerHTML != "") {
+      alert("Maximum Letters")
+    } else if (guesses.length <= wordInLetters.length && e.target.innerHTML != "") {
+      guessWrite(e, guesses);
+      this.innerHTML = "";
+    }
+  });
+}
+
+//write to preview box function and update guessArray
+function guessWrite(e, guesses){
   t = document.createTextNode(e.target.innerText);
+  var para = document.createElement("row");
   para.appendChild(t);
   guesses.push(t);
+  console.log(guesses);
+  console.log(event);
   document.getElementById("guessPreview").appendChild(para);
 }
+
+//Undo button
+myUndoButton.addEventListener('click', function(e){
+  for (var i = 0; i < wordInLetters.length; i++) {
+    myJumbledPanel[i].innerHTML = (wordInLetters[i]);
+    myGuessPreview.innerText = "";
+  }
+  guesses = [];
 });
-}
-
-
 
 
 
