9ab4477672e3e8fc22261764ea9d7ad78a7193da
manibrar <manvirbrar1988@gmail.com>
2018-06-19 10:46:24 +0100

    just incase

--- js/index2.js
+++ js/index2.js
@@ -6,9 +6,12 @@ var myJumbledPanel = document.querySelectorAll('.panel1');
 var myGuessPreview = document.getElementById('guessPreview');
 var mySubmitButton = document.getElementById('submitButton')
 var myUndoButton = document.getElementById('undoButton')
-var wordLibrary = ["WordJumble"];
-var wordInLetters = ["W", "O", "R", "D", "J", "U", "M", "B", "L", "E"];
+var wordLibrary = ["JumbleWord"];
+var wordInLetters = wordSplit(wordLibrary);
 var guesses = [];
+// var wordInLetters = ["W", "O", "R", "D", "J", "U", "M", "B", "L", "E"];
+
+
 
 //Start button and transfer letters into panels
 myStartButton.addEventListener('click', function(e) {
@@ -33,18 +36,18 @@ for (var i = 0; i < myJumbledPanel.length; i++) {
 }
 
 //write to preview box function and update guessArray
-function guessWrite(e, guesses){
-  t = document.createTextNode(e.target.innerText);
+function guessWrite(e, guesses) {
+  t = document.createTextNode(e.target.innerHTML.toLowerCase());
   var para = document.createElement("row");
+  guesses.push(t.textContent);
   para.appendChild(t);
-  guesses.push(t);
-  console.log(guesses);
-  console.log(event);
-  document.getElementById("guessPreview").appendChild(para);
+  // console.log(guesses);
+  // console.log(event);
+  myGuessPreview.appendChild(para);
 }
 
 //Undo button
-myUndoButton.addEventListener('click', function(e){
+myUndoButton.addEventListener('click', function(e) {
   for (var i = 0; i < wordInLetters.length; i++) {
     myJumbledPanel[i].innerHTML = (wordInLetters[i]);
     myGuessPreview.innerText = "";
@@ -52,8 +55,46 @@ myUndoButton.addEventListener('click', function(e){
   guesses = [];
 });
 
+//Submit and check for win
+mySubmitButton.addEventListener('click', function(e) {
+  if (guesses === wordInLetters) {
+    alert('you win');
+  } else {
+    alert('not enough characters');
+  }
+});
 
 
+var findOne = function(wordInLetters, guesses) {
+  words2 = wordInLetters;
+  guess2 = guesses;
+  return words2.some(function(v) {
+    return guess2.indexOf(v) >= 0;
+  });
+};
+
+
+function wordSplit(wordLibrary){
+  for (var i = 0; i < wordLibrary.length; i++) {
+    a = [];
+    a = wordLibrary[i];
+    return a;
+  }
+}
+
+
+//check for win
+// function checkWin() {
+//   for (var i = 0; i < wordInLetters.length; i++) {
+//     var myGuesses = guesses[i];
+//     var myWordInLetters = wordInLetters[i];
+//     for (var j = 0; i < wordInLetters[i].length; i++) {
+//       if (myWordInLetters[j] === myGuesses[j]) {
+//         alert('you win');
+//       }
+//     }
+//   }
+// }
 
 //
 // for (var i = 0; i < myJumbledPanel.length; i++) {
